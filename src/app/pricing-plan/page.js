790b4409fe98ee617336c6f7d65e85c6e464470a
saudkhanbpk <saudkhanbpk@gmail.com>
2023-07-12 16:45:30 +0500

    registration successfull pricing plane

---
+++ src/app/pricing-plan/page.js
@@ -0,0 +1,252 @@
+'use client'
+import { Box, Button,Typography,Divider } from '@mui/material'
+import Image from 'next/image'
+import React from 'react'
+
+const Registered = () => {
+    return (
+     <Box>   
+<Box sx={{display:"flex",mt:'50px', justifyContent:'center', mb:'10px'}}>
+  <Image 
+  alt="Amplify" 
+  src="/image/logo.png" 
+  width={125} height={40}
+  />
+  </Box>
+
+        <Box sx={{display:'flex',justifyContent:'center',fontFamily:'Plus Jakarta Sans'}}>
+          <Box sx={{ width: '340px',borderRadius:'16px',marginTop:'30px',fontFamily:'Plus Jakarta Sans',
+            backgroundColor:"#F9FAFF",
+            px: 2,py:2,mr:'10px'}}>
+                   <Image sx={{position:'absolute',}}
+                            alt="Amplify"
+                            src="/image/pricelogo.png"
+                            width={23}
+                            height={13}
+                        />
+                        <Typography variant="h4" sx={{fontFamily:'Plus Jakarta Sans',marginTop:'7px',marginBottom:'2px',fontSize:'32',fontWeight:'700'}}>
+                0$
+              </Typography>
+              <Typography variant="h6" sx={{fontFamily: 'Plus Jakarta Sans',fontWeight:'700',fontSize:'18',marginBottom:'2px'}}>
+                Free trial
+              </Typography>
+              <Typography variant="body1" sx={{fontFamily:'Plus Jakarta Sans',fontWeight:'400',fontSize:'14',color:'#6C737F'}}>
+                to familiarize yourself with our tools
+              </Typography>
+
+              <Divider sx={{my:2}}/>
+             
+             <Box >
+             <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (limited)
+              </Typography>
+              </Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (limited)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influen (limited)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Finder (limited)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to Analyzer (limited)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (limited)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (limited)
+              </Typography></Box>
+              
+            </Box>
+            <Box
+                    sx={{
+                            display: 'flex',
+                            alignItems: "center",
+                            justifyContent: "center",
+                            mt:4,
+                            mb:2
+                                                        
+                        }}
+                    >
+                        <Button variant="outlined" color="primary" sx={{width:'90%',color:'#6366F1',border:'1px solid #6366F1',borderRadius:'10px'}}>
+                            Start free Trial
+                        </Button>
+                    </Box>
+              
+   </Box>
+
+
+   <Box sx={{ width: '340px',borderRadius:'16px',marginTop:'30px',ml:'10px',
+    backgroundColor:"#F9FAFF",
+            px: 2,py:2}}>
+                   <Image sx={{position:'absolute',}}
+                            alt="Amplify"
+                            src="/image/pricelogo.png"
+                            width={23}
+                            height={13}
+                        />
+               <Typography variant="h4" sx={{fontFamily:'Plus Jakarta Sans',marginTop:'7px',marginBottom:'2px',fontSize:'32',fontWeight:'700'}}>
+                $99  
+                <Typography variant="body" sx={{fontSize:'14px',color:'#6C737F'}}> /mo</Typography>
+              </Typography>
+              <Typography variant="h6" sx={{fontFamily: 'Plus Jakarta Sans',fontWeight:'700',fontSize:'18',marginBottom:'2px'}}>
+                Standard
+              </Typography>
+              <Typography variant="body1" sx={{fontFamily:'Plus Jakarta Sans',fontWeight:'400',fontSize:'14',color:'#6C737F'}}>
+                to familiarize yourself with our tools
+              </Typography>
+              <Divider sx={{my:2}}/>
+             
+             <Box>
+             <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography>
+              </Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              <Box sx={{display:'flex',justifyContent:'between',my:1}}>
+             <Image sx={{}}
+                            alt="Amplify"
+                            src="/image/icon.png"
+                            width={16}
+                            height={11}
+                        /> 
+              <Typography variant="body2" sx={{fontFamily:'plus jakarta sans',fontSize:'14',ml:2}}>
+                Access to influencer Analyzer (full)
+              </Typography></Box>
+              
+            </Box>
+            <Box
+                    sx={{
+                            display: 'flex',
+                            alignItems: "center",
+                            justifyContent: "center",
+                            mt:4,
+                            mb:2
+                                                        
+                        }}
+                    >
+                        <Button variant="outlined" color="primary" sx={{width:'90%',color:'#6366F1',border:'1px solid #6366F1',borderRadius:'10px'}}>
+                            Start free Trial
+                        </Button>
+                    </Box>
+              
+   </Box>
+
+
+
+   
+        </Box>
+
+        </Box>
+    )
+}
+
+export default Registered
