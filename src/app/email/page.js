365f33c1a7796593beaf0a0b7cd14ed4ba17c157
saudkhanbpk <saudkhanbpk@gmail.com>
2023-07-21 14:58:59 +0500

    core fixes of UI, dashboard, and youtube,insta f

--- src/app/email/page.js
+++ src/app/email/page.js
@@ -1,89 +1,91 @@
-'use client'
-import { Box, Button } from '@mui/material'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { Box, Button,Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
 
 const confirEmail = () => {
-    return (
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: 20,
+        }}
+      >
         <div>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: "center",
-                    mt: 20,
-
-                }}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              alt="Amplify"
+              src="/image/logo.png"
+              width={125}
+              height={40}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              mt: 3,
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                color: "#111927",
+                fontSize: "32px",
+                fontWeight: "600",
+                fontFamily: "Plus Jakarta Sans",
+                lineHeight: "38.4px",
+              }}
             >
-                <div>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: "center",
-                        }}
-                    >
-                        <Image
-                            alt="Amplify"
-                            src="/image/logo.png"
-                            width={125}
-                            height={40}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            color: '#111927',
-                            display: 'flex',
-                            alignItems: "center",
-                            justifyContent: "center",
-                            fontWeight: 'bold',
-                            mt: 3,
-                            fontSize:32
-
-                        }}
-                    >
-                        <h4>Confirm your email address</h4>
-                    </Box>
-                    <Box
-                        sx={{
-                            color: '#6C737F',
-                            mt: 3,
-                            fontSize:18
-                        }}
-                    >
-                        <p>Please verify your email address by clicking the link sent to
-                        </p>
-
-                    </Box>
-                    <Box
-                        sx={{
-                            color: '#111927',
-                            display: 'flex',
-                            alignItems: "center",
-                            justifyContent: "center",
-                            fontSize:18
-                        }}
-                    >
-                        <p>john.doe@gmail.com</p>
-                    </Box>
-                    <Box
-                    sx={{
-                            display: 'flex',
-                            alignItems: "center",
-                            justifyContent: "center",
-                            mt:3,
-                            
-                        }}
-                    >
-                        <Button variant="contained" color="primary">
-                            Resend verification link
-                        </Button>
-                    </Box>
-                </div>
-
-            </Box>
+              Confirm your email address
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              color: "#6C737F",
+              mt: 3,
+              fontSize: 18,
+            }}
+          >
+            <p>Please verify your email address by clicking the link sent to</p>
+          </Box>
+          <Box
+            sx={{
+              color: "#111927",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: 18,
+            }}
+          >
+            <p>john.doe@gmail.com</p>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              mt: 3,
+            }}
+          >
+            <Button variant="contained" color="primary">
+              Resend verification link
+            </Button>
+          </Box>
         </div>
-    )
-}
+      </Box>
+    </div>
+  );
+};
 
-export default confirEmail
+export default confirEmail;
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,229 +1,290 @@
-'use client'
-import ArrowLeftIcon from '@untitled-ui/icons-react/build/esm/ArrowLeft';
+"use client";
+import ArrowLeftIcon from "@untitled-ui/icons-react/build/esm/ArrowLeft";
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    Container,
-    SvgIcon,
-    Link,
-    TextField,
-    Typography
-} from '@mui/material';
-import { baseUrl } from '../BaseUrl';
-import Checkbox from '@mui/material/Checkbox';
-import Header from '../header/page';
-import { useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/navigation'
-import { headers } from 'next/dist/client/components/headers';
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  SvgIcon,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+import { baseUrl } from "../BaseUrl";
+import Checkbox from "@mui/material/Checkbox";
+import Header from "../header/page";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { headers } from "next/dist/client/components/headers";
 // import Link from 'next/link'
 
 const Register = () => {
-    console.log("baseurl:", baseUrl)
-    const router = useRouter()
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const data = {
-            name: name,
-            email: email,
-            password: password,
-            confirmPassword: confirmPassword
-        };
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };
-        axios.post(`${baseUrl}/api/register`, data, config)
-            .then((response) => {
-                console.log(response.data); // Handle the successful response
-            })
-            .catch((error) => {
-                console.error(error); // Handle the error
-            });
-        router.push('/email')
-
-    }
-    return (
-        <>
-            <Header />
-            <Box
-                sx={{
-                    backgroundColor: (theme) => theme.palette.mode === 'dark'
-                        ? 'neutral.800'
-                        : 'neutral.100',
-                    p: 3
-                }}
+  console.log("baseurl:", baseUrl);
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axios
+      .post(`${baseUrl}/api/register`, data, config)
+      .then((response) => {
+        console.log(response.data); // Handle the successful response
+      })
+      .catch((error) => {
+        console.error(error); // Handle the error
+      });
+    router.push("/email");
+  };
+  return (
+    <>
+      <Header />
+      <Box
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "dark" ? "neutral.800" : "neutral.100",
+          p: 3,
+        }}
+      >
+        <Container maxWidth="xs">
+          <div>
+            <Link
+              color="text.primary"
+              // component={RouterLink}
+              // href={paths.index}
+              sx={{
+                alignItems: "center",
+                display: "inline-flex",
+                color: "#111927",
+                pb: 2,
+                textDecoration:'none'
+               
+              }}
+              // underline="hover"
             >
+              <SvgIcon sx={{ mr: 1 }}>
+                <ArrowLeftIcon />
+              </SvgIcon>
+              <Typography variant="subtitle2" >Back</Typography>
+            </Link>
+          </div>
+          <Box
+            sx={{
+              fontSize: 25,
+              pb: 4,
+              fontWeight: "bold",
+              color: "#111927",
+              width:'444px'
+            }}
+          >
+            <Typography
+              variant="H4"
+              sx={{
+                fontSize: "32px",
+                fontFamily: "Plus Jakarta Sans",
+                fontWeight: 700,
+                lineHeight: "38.4px",
+                color: "#111927",
+              }}
+            >
+              Brand account creation
+            </Typography>
+          </Box>
+          <Card
+            sx={{
+              borderRadius: "16px",
+              boxShadow: 2,
+            }}
+          >
+            <CardContent
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: 400,
+                p: 4,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  // fontFamily: "Inter",
+                }}
+              >
+                <div>
+                  <Typography
+                    variant="H6"
+                    sx={{
+                      fontSize: "18px",
+                      fontFamily: "Plus Jakarta Sans",
+                      fontWeight: 700,
+                      lineHeight: "21.4px",
+                      color: "#111927",
+                    }}
+                  >
+                    Register
+                  </Typography>
+                  <Box
+                    sx={{
+                      mt: 1,
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <Typography
+                      
+                      sx={{
+                        fontSize: "14px",
+                        fontWeight: 400,
+                        lineHeight: "22px",
+                        color: "#6C737F",
+                      }}
+                    >
+                      Already have an account? 
+                    </Typography>
+                    <Typography sx={{
+                        fontSize: "14px",
+                        fontWeight: 400,
+                        lineHeight: "22px",
+                        color: "#2970FF",
+                        pl:'4px'
+                      }}>Log in</Typography>
+                  </Box>
+                </div>
+              </Box>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  mt: 3,
+                }}
+              >
+                <form onSubmit={handleSubmit}>
+               
+                  <TextField
+                    fullWidth
+                    label="Username"
+                    margin="normal"
+                    name="name"
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                   sx={{borderRadius:'12px'}}
 
-                <Container maxWidth="xs">
-                    <div>
-                        <Link
-                            color="text.primary"
-                            // component={RouterLink}
-                            // href={paths.index}
-                            sx={{
-                                alignItems: 'center',
-                                display: 'inline-flex',
-                                color: '#111927',
-                                pb: 2
-                            }}
-                        // underline="hover"
-                        >
-                            <SvgIcon sx={{ mr: 1 }}>
-                                <ArrowLeftIcon />
-                            </SvgIcon>
-                            <Typography variant="subtitle2">
-                                Back
-                            </Typography>
-                        </Link>
-                    </div>
-                    <Box sx={{
-                        fontSize: 25,
-                        pb: 4,
-                        fontWeight: 'bold',
-                        color: "#111927"
-                    }}>
-                        <h4>Brand account creation</h4>
-                    </Box>
-                    <Card sx={{
-                        borderRadius: '16px',
-                        boxShadow: 2
-                    }}>
-                        <CardContent
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                minHeight: 400,
-                                p: 4,
+                   />
+                  <TextField
+                    fullWidth
+                    label="Email Address"
+                    margin="normal"
+                    name="email"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    fullWidth
+                    label="Password"
+                    margin="normal"
+                    name="password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <TextField
+                    fullWidth
+                    label="Confirm Password"
+                    margin="normal"
+                    name="password"
+                    type="password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
 
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    fontFamily: 'Inter',
+                  <Box
+                    sx={{
+                      mt: 2,
+                      ml: 2,
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <Checkbox variant='subtitle2' name="policy" />
+                    <Box
+                    sx={{
+                      mt: 1,
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        fontSize: "14px",
+                        // fontFamily: "Inter",
+                        fontWeight: 400,
+                        lineHeight: "22px",
+                        color: "#6C737F",
+                      }}
+                    >
+                      I have read the 
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                     sx={{
+                        fontSize: "16px",
+                        // fontFamily: "Inter",
+                        fontWeight: 400,
+                        lineHeight: "24px",
+                        color: "#2970FF",
+                        pl:'4px'
+                      }}>Terms and Conditions</Typography>
+                  </Box>
+                  </Box>
+                  <Box
+                    sx={{
+                      mt: 2,
+                      ml: 2,
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <Checkbox variant='subtitle2' name="policy" />
+                    <Typography variant="body2" 
+                    sx={{fontSize: "14px",
+                        // fontFamily: "Inter",
+                        fontWeight: 400,
+                        lineHeight: "22px",
+                        color: "#6C737F",}}>
+                      I’m not a robot
+                    </Typography>
+                  </Box>
 
-                                }}
-                            >
-                                <div>
-                                    <Typography variant="h4" sx={{ color: "#111927", fontWeight: 'bold' }}>
-                                        <h6>Register</h6>
-                                    </Typography>
-                                    <Box sx={{
-                                        mt: 1,
-                                        alignItems: 'center',
-                                        display: 'flex',
-                                    }}>
-                                        <Typography
-                                            color="text.secondary"
-                                            variant="body2"
-                                        >
-                                            Already have an account?
-                                            {' '}
-                                            <Link href="#">
-                                                Log in
-                                            </Link>
-                                        </Typography>
-                                    </Box>
-                                </div>
-
-                            </Box>
-                            <Box
-                                sx={{
-                                    flexGrow: 1,
-                                    mt: 3
-                                }}
-                            >
-                                <form onSubmit={handleSubmit}>
-                                    <TextField
-                                        fullWidth
-                                        label="Username"
-                                        margin="normal"
-                                        name="name"
-                                        type="text"
-                                        onChange={(e) => setName(e.target.value)}
-
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Email Address"
-                                        margin="normal"
-                                        name="email"
-                                        type="email"
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Password"
-                                        margin="normal"
-                                        name="password"
-                                        type="password"
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Confirm Password"
-                                        margin="normal"
-                                        name="password"
-                                        type="password"
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                    />
-
-                                    <Box sx={{
-                                        mt: 2, ml: 2,
-                                        alignItems: 'center',
-                                        display: 'flex',
-                                    }}>
-                                        <Checkbox name="policy" />
-                                        <Typography
-                                            color="text.secondary"
-                                            variant="body2"
-                                        >
-                                            I have read the
-                                            {' '}
-                                            <Link href="#">
-                                                Terms and Conditions
-                                            </Link>
-                                        </Typography>
-                                    </Box>
-                                    <Box sx={{
-                                        mt: 2, ml: 2,
-                                        alignItems: 'center',
-                                        display: 'flex',
-                                    }}>
-                                        <Checkbox name="policy" />
-                                        <Typography
-                                            color="text.secondary"
-                                            variant="body2"
-                                        >
-                                            I’m not a robot
-
-                                        </Typography>
-                                    </Box>
-
-                                    <Box sx={{ mt: 2 }}>
-                                        <Button
-                                            fullWidth
-                                            size="large"
-                                            type="submit"
-                                            variant="contained"
-                                            color="primary"
-
-                                        >
-                                            Register
-                                        </Button>
-                                    </Box>
-                                </form>
-                            </Box>
-                            {/* <Divider sx={{ my: 3 }} />
+                  <Box sx={{ mt: 2 }}>
+                    <Button
+                      fullWidth
+                      // size="large"
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      sx={{backgroundColor:'#2970FF',borderRadius:'10px',textDecoration:'none'}}
+                    >
+                      Register
+                    </Button>
+                  </Box>
+                </form>
+              </Box>
+              {/* <Divider sx={{ my: 3 }} />
                     <Link
                         color="text.secondary"
                         href="#"
@@ -231,12 +292,12 @@ const Register = () => {
                     >
                         Create new account
                     </Link> */}
-                        </CardContent>
-                    </Card>
-                </Container>
-            </Box>
-        </>
-    )
+            </CardContent>
+          </Card>
+        </Container>
+      </Box>
+    </>
+  );
 };
 
-export default Register
+export default Register;
