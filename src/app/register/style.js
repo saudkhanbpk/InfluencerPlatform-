95b51eaabb42c8ff9afa094084f73fb50e7c01f5
saud khan <saudkhanbpk@gmail.com>
2023-07-21 00:03:22 -0700

    rec done

--- src/app/register/style.js
+++
@@ -1,8 +0,0 @@
-const { styled, Box } = require("@mui/material");
-
-export const BrandBox = styled(Box)(({ theme }) => ({
-    fontSize: 25,
-    // pb: 4,
-    fontWeight: 'bold',
-    color: "#111927"
-}))
