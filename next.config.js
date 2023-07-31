9c9c51920410318237e54f4b2066af5bfd4af373
saud khan <saudkhanbpk@gmail.com>
2023-07-31 02:59:23 -0700

    next.config file updated for deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
+// next.config.js
+const nextConfig = {
+  distDir: 'build', // Use 'build' as the custom build directory
+};
 
 module.exports = nextConfig
